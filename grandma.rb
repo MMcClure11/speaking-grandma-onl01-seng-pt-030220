943e47c2291358f760347cbe8c0a3c346a60c3bc
Megan Christine McClure <mcclme02@luther.edu>
2020-03-04 01:25:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,8 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+  if phrase == "I love you grandma!".upcase
+    return "I love you too pumpkin!".upcase
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
